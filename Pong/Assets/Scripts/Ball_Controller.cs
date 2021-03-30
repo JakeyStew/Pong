fc59d8ba01e4787af0c1b590e9d50a9f26495735
Jake Stewart <42218259+JakeyStew@users.noreply.github.com>
2021-03-30 11:12:24 +0100

    Basic Movement and Ball bounce

---
+++ Pong/Assets/Scripts/Ball_Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Controller : MonoBehaviour
+{
+    private Rigidbody2D ballRb;
+    private Vector3 lastVelocity;
+    [SerializeField]
+    private Vector2 force = new Vector2(5.0f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRb = GetComponent<Rigidbody2D>();
+        if(ballRb == null)
+        {
+            Debug.LogError("Ball GameObject missing Rigidbody2D!");
+        }
+        ballRb.AddForce(force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastVelocity = ballRb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        Debug.Log("Collision : " + collider);
+        if(collider.gameObject != null)
+        {
+            var speed = lastVelocity.magnitude;
+            var direction = Vector3.Reflect(lastVelocity.normalized, collider.contacts[0].normal);
+            ballRb.velocity = direction * Mathf.Max(speed, 0f);
+        }
+    }
+}
---
+++ Pong/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    [SerializeField]
+    private Camera MainCamera;
+    [SerializeField]
+    float _moveSpeed = 100;
+    private Vector2 _screenBounds;
+    private float objectHeight;
+
+    void Start()
+    {
+        //Gets the boundaries of the camera view.
+        _screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        //Gets the hieght of the seletced game object
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
+
+    private void Update()
+    {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        if (gameObject.tag == "Player") { 
+            //Get input for vertical movement
+            float verticalInput = Input.GetAxis("Vertical");
+
+            //Set the movement speed and axis
+            var movement = new Vector3(0, verticalInput, 0) * _moveSpeed * Time.deltaTime;
+            transform.position += movement;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.y = Mathf.Clamp(viewPos.y, _screenBounds.y * -1 + objectHeight, _screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
